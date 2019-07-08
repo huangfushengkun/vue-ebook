73f14d560c1acc256c25f05fc29fdeeb205b158f
xzh.hf <xzh.hf@hangshutech.com>
2019-07-08 18:31:06 +0800

    add rem

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,23 @@
     <router-view/>
   </div>
 </template>
+<script>
+  document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('html')
+    let fontSize = window.innerWidth / 10
+    fontSize = fontSize > 50 ? 50 : fontSize
+    html.style.fontSize = fontSize + 'px'
+  })
+export default {
+  
+}
+</script>
+
 
 <style scoped>
   .text {
     font-family: 'Days One';
-    font-size: 20px;
+    font-size: 1rem;
     color: red;
   }
 </style>
