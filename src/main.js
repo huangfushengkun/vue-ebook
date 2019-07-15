4b34a7ed7b403fae1db2bd6330804282b3f62554
xzh.hf <xzh.hf@hangshutech.com>
2019-07-15 09:29:06 +0800

    add setmessage

--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import store from './store/index'
 import './assets/styles/icon.css'
 // import './assets/fonts/daysOne.css'
 import './assets/styles/global.scss'
-
+import i18n from './lang/index.js'
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
